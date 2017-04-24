fe76dfcddc168d6f458ec76420b6263441434a42
Forrest Chang <fchang@hedgeye.com>
2017-04-24 15:51:24 -0700

    Make start code w/more to refactor

--- simple_refactoring_exercise/java/CodeReplacer.java
+++ simple_refactoring_exercise/java/CodeReplacer.java
@@ -6,64 +6,67 @@ import java.util.*;
 public class CodeReplacer {
     public final String TEMPLATE_DIR = "templatedir";
     String sourceTemplate;
+    String code;
+    String altcode;
     /**
      * @param reqId java.lang.String
      * @param oStream java.io.OutputStream
      * @exception java.io.IOException The exception description.
      */
-
-    private String readTemplate() {
-	String templateName = System.getProperty(TEMPLATE_DIR, "") + "template.html";
-	StringBuffer sb = new StringBuffer("");
-	BufferedReader br = null;
-	try {
-	    br = new BufferedReader( new FileReader( templateName ));
-	    String line;
-	    while(((line=br.readLine())!= null)) {
-		sb.append( line);
-		sb.append( "\n");
-	    }
-	    
-	    br.close();
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.out.println( "exception caught" + e.getCause() +e.getMessage());
-	} finally {
-	    try {
-		br.close();
-	    } catch( IOException ignored) { // coz u can't do anything 
-	    }
-	}
-	return sb.toString();
-    }
-
-
-    private String substituteForCode( String template, String reqId, String substitutablePattern) {
-	int templateSplitBegin = template.indexOf( substitutablePattern);
-	int templateSplitEnd = templateSplitBegin + substitutablePattern.length();
-	String templatePartOne = template.substring(0, templateSplitBegin);
-	String templatePartTwo = template.substring(templateSplitEnd, template.length());
-	return templatePartOne+reqId+templatePartTwo;
-    }
-
-
     public void substitute(String reqId, PrintWriter out)
 	throws IOException
-    {
-	sourceTemplate = readTemplate();
-	try {
-	    String template = sourceTemplate;
-	    template = substituteForCode( template, reqId, "%CODE%");
-	    String result = substituteForCode( template,
-                                               reqId.substring(0,5) + "-" + reqId.substring(5,8),
-                                               "%ALTCODE%");
-	    out.print( result);
-	} catch (Exception e) {
-	    System.out.println("Error in substitute()");
-	}
-	out.flush();
-	out.close();
-    }
+        {
+            // Read in the template file
+            String templateDir = System.getProperty(TEMPLATE_DIR, "");
+            StringBuffer sb = new StringBuffer("");
+            try {
+                FileReader fr = new FileReader(templateDir +
+                                               "template.html");
+                BufferedReader br = new BufferedReader(fr);
+                String line;
+                while(((line=br.readLine())!="") && line!=null)
+                    sb = new StringBuffer(sb + line + "\n");
+                br.close();
+                fr.close();
+            } catch (Exception e) {
+	    
+                e.printStackTrace();
+	    
+                System.out.println( "exception caught" + e.getCause() +e.getMessage());
+	    
+            }
+            sourceTemplate = new String(sb);
+            try {
+                String template = new String(sourceTemplate);
+                // Substitute for %CODE%
+                int templateSplitBegin = template.indexOf("%CODE%");
+                int templateSplitEnd = templateSplitBegin + 6;
+                String templatePartOne = new String(
+                    template.substring(0, templateSplitBegin));
+                String templatePartTwo = new String(
+                    template.substring(templateSplitEnd,
+                                       template.length()));
+                code = new String(reqId);
+                template = new String(
+                    templatePartOne+code+templatePartTwo);
+                // Substitute for %ALTCODE%
+                templateSplitBegin = template.indexOf("%ALTCODE%");
+                templateSplitEnd = templateSplitBegin + 9;
+                templatePartOne = new String(
+                    template.substring(0, templateSplitBegin));
+                templatePartTwo = new String(
+                    template.substring(templateSplitEnd,
+                                       template.length()));
+                altcode = code.substring(0,5) + "-" +
+                    code.substring(5,8);
+                out.print(templatePartOne+altcode+templatePartTwo);
+            } catch (Exception e) {
+                System.out.println("Error in substitute()");
+            }
+            out.flush();
+            out.close();
+        }
 }
+
 
 
